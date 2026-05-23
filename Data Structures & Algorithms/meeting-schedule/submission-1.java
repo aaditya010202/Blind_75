456c8921ea9bfe3518925fd7e34384097982a0f4
Aaditya Bhatnagar <72307363+aaditya010202@users.noreply.github.com>
2026-05-23 22:28:08 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.java
@@ -0,0 +1,23 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        boolean ans = true;
+        Collections.sort(intervals, Comparator.comparingInt(i -> i.start));
+        for(int i=1;i<intervals.size();i++) {
+            Interval interval1 = intervals.get(i-1);
+            Interval interval2 = intervals.get(i);
+            if(interval1.end > interval2.start) return false;
+        }
+        return ans;
+    }
+}
